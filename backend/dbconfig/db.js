b69f24cfc4764c6e34157f3bd27060ee8f871b1b
sarmaasis <sarmaasis@gmail.com>
2022-10-16 04:48:41 +0530

    connected DB to cloud DB MongoDB Atlas

--- backend/dbconfig/db.js
+++ backend/dbconfig/db.js
@@ -1,5 +1,11 @@
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/movie_search';
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-module.exports = mongoose;
+// const mongoose = require('mongoose');
+// const mongoDB = 'mongodb://localhost/movie_search';
+// mongoose.connect(mongoDB);
+// mongoose.Promise = global.Promise;
+// module.exports = mongoose;
+
+const Config = {
+    dbUrl: "mongodb+srv://sarmaasis:Nit&&iit1234@db.hvauc1w.mongodb.net/?retryWrites=true&w=majority"
+}
+
+module.exports = Config
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const port = "8000"
-const mongoose = require('./dbconfig/db');
+const mongoose = require('mongoose');
 const router = require('./router/route');
+const db = require('./dbconfig/db');
 
 const app = express()
 app.use(cors())
@@ -14,7 +15,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connect(db.dbUrl, (err) => {
+    if(err){
+        console.log(err)
+    }
+});
 
 
 app.use('/api', router)
